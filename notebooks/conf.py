f4d6d22d633030b52495f1acdce1c9286df8c345
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2021-02-07 19:38:08 +0100

    fix sphinx build

--- notebooks/conf.py
+++ notebooks/conf.py
@@ -14,11 +14,15 @@ needs_sphinx = "1.6"
 
 master_doc = "index"
 
-source_suffix = ['.rst']
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.ipynb': 'jupyter_notebook',
+    '.md': 'jupyter_notebook'
+}
 
 exclude_patterns = ['_build', 'build', '**.ipynb_checkpoints']
 
-extensions = ['ipypublish.sphinx.notebook', 'ipypublish.sphinx.gls', 'sphinx.ext.mathjax', 'sphinxcontrib.bibtex', 'sphinx.ext.todo']
+extensions = ['ipypublish.sphinx.notebook', 'ipypublish.sphinx.gls', 'sphinx.ext.mathjax', 'sphinx.ext.todo']
 
 ### where to find our custom material
 # conf file itself
